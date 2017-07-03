77cd4f3562632e871ea9201ab4f3e6997296ca42
Cristhian Kevin Sagun <cristhiansagun@gmail.com>
2017-07-03 09:03:41 +0800

    Update master branch

---
+++ application/config/calendar.php
@@ -0,0 +1,10 @@
+<?php
+
+$config['template'] = array(
+	'table_open'           => '<table class="calendar">',
+	'cal_cell_start'       => '<td class="day">',
+	'cal_cell_start_today' => '<td class="today">'
+	'cal_cell_start'       => '<td class="day">',
+	'cal_cell_start_today' => '<td class="today">'
+	'table_close'		   => '</table>'
+);
--- application/views/components/main-footer.php
+++ application/views/components/main-footer.php
@@ -4,7 +4,7 @@
         	Page rendered in <strong>{elapsed_time}</strong> seconds.
             Human Resource Information System [DEV:PROTOTYPE_VERSION].
         </p> -->
-        <img src="http://localhost/kawani_ci/assets/img/app/lohica.png" alt="Image" width="150" height="30">
+        <img src="<?php echo site_url('assets/img/app/lohica.png'); ?>" alt="Image" width="150" height="30">
     </div>
-    <strong>&copy; <?php echo date('Y') ?> <img src="http://localhost/kawani_ci/assets/img/app/systemantech.png" alt="Image" width="150" height="30"></strong> 
+    <strong>&copy; <?php echo date('Y') ?> <img src="<?php echo site_url('assets/img/app/systemantech.png'); ?>" alt="Image" width="150" height="30"></strong>
 </footer>
